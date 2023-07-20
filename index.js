d0f65d85219efb883b81a1b52a75649862ad246b
anshjain <anshjain@expediagroup.com>
2023-07-20 15:03:54 +0530

    Delta changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const memcachedClient = new memcached(`${process.env.ENDPOINT}:${process.env.POR
 exports.chargeRequestRedis = async function (input) {
     const redisClient = await getRedisClient();
     var remainingBalance = await getBalanceRedis(redisClient, KEY);
-    var charges = getCharges();
+    var charges = getCharges(input);
     const isAuthorized = authorizeRequest(remainingBalance, charges);
     if (!isAuthorized) {
         return {
@@ -39,32 +39,47 @@ exports.resetRedis = async function () {
             }
         });
     });
+    await ret;
     await disconnectRedis(redisClient);
     return ret;
 };
 exports.resetMemcached = async function () {
     var ret = new Promise((resolve, reject) => {
-        memcachedClient.set(KEY, DEFAULT_BALANCE, MAX_EXPIRATION, (res, error) => {
-            if (error)
-                resolve(res);
-            else
-                reject(DEFAULT_BALANCE);
+        memcachedClient.add(KEY, DEFAULT_BALANCE, MAX_EXPIRATION, (error) => {
+            if (!error) {
+            
+                console.log("Set Normally");
+                resolve(DEFAULT_BALANCE);
+            }
+            else {
+                console.log("Set via CAS");
+                memcachedClient.gets(KEY, function (err, data) {
+                memcachedClient.cas(KEY, DEFAULT_BALANCE, data.cas, MAX_EXPIRATION, function (err) {  });
+                });
+           
+                resolve(DEFAULT_BALANCE);
+            }
         });
     });
+    await ret;
     return ret;
 };
 exports.chargeRequestMemcached = async function (input) {
     var remainingBalance = await getBalanceMemcached(KEY);
-    const charges = getCharges();
-    const isAuthorized = authorizeRequest(remainingBalance, charges);
-    if (!authorizeRequest(remainingBalance, charges)) {
+    console.log(remainingBalance.value);
+    //console.log(remainingBalance.cas);
+    const charges = input.unit;
+    console.log(charges);
+    const isAuthorized = authorizeRequest(remainingBalance.value, charges);
+    if (!isAuthorized) {
         return {
             remainingBalance,
             isAuthorized,
             charges: 0,
         };
     }
-    remainingBalance = await chargeMemcached(KEY, charges);
+   
+    await chargeMemcached(KEY, remainingBalance.value - charges, remainingBalance.cas);
     return {
         remainingBalance,
         charges,
@@ -107,8 +122,9 @@ async function disconnectRedis(client) {
 function authorizeRequest(remainingBalance, charges) {
     return remainingBalance >= charges;
 }
-function getCharges() {
-    return DEFAULT_BALANCE / 20;
+function getCharges(input) {
+    console.log(input.unit);
+    return input.unit;
 }
 async function getBalanceRedis(redisClient, key) {
     const res = await util.promisify(redisClient.get).bind(redisClient).call(redisClient, key);
@@ -119,25 +135,41 @@ async function chargeRedis(redisClient, key, charges) {
 }
 async function getBalanceMemcached(key) {
     return new Promise((resolve, reject) => {
-        memcachedClient.get(key, (err, data) => {
+        memcachedClient.gets(key, (err, data) => {
             if (err) {
                 reject(err);
             }
             else {
-                resolve(Number(data));
+                console.log(data);
+                resolve({value : Number(data[key]), cas: data.cas});
             }
         });
     });
 }
-async function chargeMemcached(key, charges) {
+async function chargeMemcached(key, balance, cas) {
     return new Promise((resolve, reject) => {
-        memcachedClient.decr(key, charges, (err, result) => {
-            if (err) {
+        console.log(cas);
+        console.log(balance);
+            memcachedClient.gets(KEY, function (err, data) {
+                memcachedClient.cas(KEY, balance, cas, MAX_EXPIRATION, function (err) { 
+                    if (err) {
+                console.log("error");
                 reject(err);
             }
             else {
-                return resolve(Number(result));
+                console.log("updated");
+                return resolve();
+            }  });
+                });
+        /*memcachedClient.cas(key, balance, cas, MAX_EXPIRATION, (err) => {
+           /* if (err) {
+                console.log("error");
+                reject(err);
             }
-        });
+            else {
+                console.log("updated");
+                return resolve();
+            }*/
+       // }); */
     });
 }
